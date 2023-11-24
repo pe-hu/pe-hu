5e1266c86d3d0480860f57b57ad08549cc1a988d
∧° ┐ <we.are.pe.hu@gmail.com>
2023-11-24 15:57:22 +0900

    index

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict'
 
+async function fetchText(url = '', query = '') {
+  fetch(url)
+  .then(response => response.text())
+  .then(html => {
+    document.querySelector(query).innerText = html
+  });
+}
+
 async function fetchHTML(url = '', query = '') {
   fetch(url)
   .then(response => response.text())
